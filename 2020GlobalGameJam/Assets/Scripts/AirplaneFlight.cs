7af5684a9de4192d859d86269c5d49d0acaae0dc
fickesjoshua <fickes@usc.edu>
2020-02-01 11:27:15 -0800

    Added some workspaces and put in sample flight versions.

---
+++ 2020GlobalGameJam/Assets/Scripts/AirplaneFlight.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirplaneFlight : MonoBehaviour
+{
+    public float baseMoveSpeed;
+    public float moveSpeedModifier;
+    public float moveSpeed;
+
+    [SerializeField]
+    float turnSpeed;
+    [SerializeField]
+    float upwardPitchMax = 80;
+    [SerializeField]
+    float downwardPitchMax = -80;
+
+    [SerializeField]
+    float initialPitchAngle;
+
+    Vector3 newPosition;
+    Rigidbody rb;
+    float currentPitchAngle = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        moveSpeed = baseMoveSpeed;
+        currentPitchAngle = initialPitchAngle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, turnSpeed * Time.deltaTime, 0, Space.World);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -1 * turnSpeed * Time.deltaTime, 0, Space.World);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if ((currentPitchAngle + (turnSpeed * Time.deltaTime)) <= upwardPitchMax)
+            {
+                transform.Rotate(turnSpeed * Time.deltaTime, 0, 0, Space.Self);
+                currentPitchAngle += (turnSpeed * Time.deltaTime);
+            }
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if ((currentPitchAngle - (turnSpeed * Time.deltaTime)) >= downwardPitchMax)
+            {
+                transform.Rotate(-1 * turnSpeed * Time.deltaTime, 0, 0, Space.Self);
+                currentPitchAngle -= (turnSpeed * Time.deltaTime);
+            }
+        }
+        newPosition = (Vector3)gameObject.transform.position + transform.forward * moveSpeed * Time.deltaTime;
+        rb.MovePosition(newPosition);
+    }
+
+    public void ChangeSpeed(int positiveOrNegative)
+    {
+        moveSpeed = moveSpeed + (moveSpeedModifier * positiveOrNegative);
+        if (moveSpeed <= 0)
+        {
+            moveSpeed = 0.1f;
+        }
+    }
+}
---
+++ 2020GlobalGameJam/Assets/Scripts/Flap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flap : MonoBehaviour
+{
+    public float downwardGravityForce;
+    public float flapForce;
+    public KeyCode flapKey;
+   
+    Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, (-1 * downwardGravityForce), 0, ForceMode.Force);
+        if (Input.GetKeyDown(flapKey))
+        {
+            rb.AddRelativeForce(0, flapForce, 0, ForceMode.Impulse);
+        }
+    }
+}
