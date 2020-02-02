ec624a2786ffdf725d4a56a2727ef205cd6fb9b5
bethanyl <bethany@usc.edu>
2020-02-01 18:53:49 -0800

    camera movement

---
+++ 2020GlobalGameJam/Assets/Scripts/Beth/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+[AddComponentMenu("Camera-Control/Smooth Mouse Look")]
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject target;
+    public float rotateSpeed = 5;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = target.transform.position - transform.position;
+    }
+
+    void LateUpdate()
+    {
+        Cursor.visible = false;
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.transform.Rotate(0, horizontal, 0);
+
+        float desiredAngle = target.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        transform.position = target.transform.position - (rotation * offset);
+
+        transform.LookAt(target.transform);
+    }
+}
+ 
