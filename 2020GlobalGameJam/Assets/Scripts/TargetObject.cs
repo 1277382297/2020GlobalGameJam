ece8be7d05aa2c81f34786e413fef2c727ed8380
fickesjoshua <fickes@usc.edu>
2020-02-01 18:20:00 -0800

    Hit Object Communicates with System Manager
    Edited TargetObject.cs to reference SystemControl.cs

--- 2020GlobalGameJam/Assets/Scripts/TargetObject.cs
+++ 2020GlobalGameJam/Assets/Scripts/TargetObject.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class TargetObject : MonoBehaviour
 {
+    [SerializeField]
+    string objectName;
+
+    SystemControl theSystemController;
     // Start is called before the first frame update
     void Start()
     {
-        
+        theSystemController = FindObjectOfType<SystemControl>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(theSystemController.totaltapelength); //Just to make sure we actually have the object.
     }
 
     public void IsHit()
     {
         Debug.Log("I am hit");
+        theSystemController.SticktoObj(objectName);
+        gameObject.GetComponent<Collider>().enabled = false;
         //DoSomething
     }
 }
