ec4c232fd0dd450401fe2477fe88db8bd699c66a
fickesjoshua <fickes@usc.edu>
2020-02-01 18:35:55 -0800

    Lives Manager

---
+++ 2020GlobalGameJam/Assets/Scripts/LivesManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivesManager : MonoBehaviour
+{
+    [SerializeField]
+    int maxLives = 7;
+
+    int currentLives = 999;
+    SystemControl theSystemController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLives = maxLives;
+        theSystemController = FindObjectOfType<SystemControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeLives(int lifeChangeAmount)
+    {
+        currentLives += lifeChangeAmount;
+        if (currentLives <= 0)
+        {
+            theSystemController.finishgame();
+        }
+    }
+}
